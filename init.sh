63e5ffa02927368c8d73af2b4e83b37d738f7684
Sean Capaloff-Jones <sean@shooflysolutions.com>
2016-08-21 23:24:31 -0400

    fix for other environments

--- init.sh
+++ init.sh
@@ -10,5 +10,6 @@ sudo apt-get install exuberant-ctags
 git clone https://github.com/joonty/vdebug.git
 git clone https://github.com/vim-php/tagbar-phpctags.vim.git
 git clone https://github.com/ap/vim/buftabline.git
+git clone https://github.com/vim-scripts/vim-misc.git
 git clone https://github.com/vim-scripts/easytags.vim.git
 
