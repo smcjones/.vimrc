844d20ee98e510e2395187dbcecff38c9d1b5bdd
Sean Capaloff-Jones <scaaloff@cs.columbia.edu>
2016-09-17 13:24:35 -0400

    cleaned up code

--- init.sh
+++ init.sh
@@ -4,13 +4,8 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim/bundle
 
-sudo apt-get install exuberant-ctags
-
 # Install dependencies
 git clone https://github.com/joonty/vdebug.git
-git clone https://github.com/ap/vim-buftabline.git
-git clone https://github.com/vim-php/tagbar-phpctags.vim.git
-git clone https://github.com/ap/vim/buftabline.git
 git clone https://github.com/vim-scripts/vim-misc.git
 git clone https://github.com/vim-scripts/easytags.vim.git
 git clone https://github.com/ap/vim-buftabline.git
