a5cc33fef7acdc4b6113857e12d0c6de8d23fcde
Sean Capaloff-Jones <scaaloff@cs.columbia.edu>
2016-09-17 13:26:37 -0400

    cleaned up more

--- init.sh
+++ init.sh
@@ -7,5 +7,4 @@ cd ~/.vim/bundle
 # Install dependencies
 git clone https://github.com/joonty/vdebug.git
 git clone https://github.com/vim-scripts/vim-misc.git
-git clone https://github.com/vim-scripts/easytags.vim.git
 git clone https://github.com/ap/vim-buftabline.git
