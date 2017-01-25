6dc4f9df592ece5e4977c1b64b813a304dd419ea
Sean Capaloff-Jones <scapaloff@cs.columbia.edu>
2017-01-25 16:02:08 -0500

    added matching for html

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 
 # Install dependencies
+git clone https://github.com/vim-scripts/matchit.zip
 git clone https://github.com/joonty/vdebug.git
 git clone https://github.com/vim-scripts/vim-misc.git
 git clone https://github.com/ap/vim-buftabline.git
